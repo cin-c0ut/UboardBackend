0936e35161ffe81c8ceef042521580cddd14e23f
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2024-12-17 21:37:07 +1100

    Start new app, migrate backend

--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Gym(models.Model):
-    id = models.BigAutoField()
-    name = models.CharField()
-    location = models.CharField()
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
 
 class Wall(models.Model):
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     gym_id = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     angle = models.FloatField()
     size = models.CharField(max_length=15)
     image = models.ImageField(upload_to='walls/')
@@ -21,32 +21,31 @@ class Wall(models.Model):
 
 class Climb(models.Model):
     wall_id = models.ForeignKey(Wall, on_delete=models.CASCADE)
-    id = models.BigAutoField()
-    name = models.CharField()
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=200)
     grade = models.CharField(max_length=15)
     rating = models.IntegerField()
     holds = models.JSONField() # Will hold array of ints corresponding to which holds will be used
                                # each int corresponds to a bounding box in Wall class
     # Not sure how to integrate how what each hold is (foot only, hand only, start, end)
         # maybe do separate class??
-    # key = ???
 
 class Saved_Gym:
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
 
 class Saved_Wall:
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     wall = models.ForeignKey(Wall, on_delete=models.CASCADE)
 
 class Saved_Climb:
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     climb = models.ForeignKey(Climb, on_delete=models.CASCADE)
 
 class Climbing_Log:
-    id = models.BigAutoField()
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     climb = models.ForeignKey(Climb, on_delete=models.CASCADE)
