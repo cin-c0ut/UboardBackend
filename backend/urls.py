b2668e60c3435c29bcc301419acaa5db4d3aaab0
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2025-01-08 14:48:17 +1100

    Fix routing error

--- backend/urls.py
+++ backend/urls.py
@@ -5,8 +5,10 @@ from backend import views
 
 router = DefaultRouter()
 router.register(r'gyms', views.GymViewSet, basename='gym')
-router.register(r'climbs', views.ClimbViewSet, basename='climbs')
-router.register(r'profiles', views.ProfileViewSet, basename='profiles')
+router.register(r'walls', views.WallViewSet, basename='wall')
+router.register(r'climbs', views.ClimbViewSet, basename='climb')
+router.register(r'profiles', views.ProfileViewSet, basename='profile')
+router.register(r'climbing_logs', views.ClimbingLogViewSet, basename='climbing_log')
 
 urlpatterns = [
     path('', include(router.urls)),
