02ad87dfa05e9694cf8e1a4c93dbbdd9713f0554
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2025-01-12 02:07:47 +1100

    Add filtering action

--- backend/views.py
+++ backend/views.py
@@ -2,7 +2,9 @@ from backend.models import Gym, Wall, Climb, Profile, Climbing_Log
 from django.contrib.auth.models import User
 from backend.serializers import GymSerializer, WallSerializer, ClimbSerializer, ProfileSerializer, ClimbingLogSerializer
 from backend.permissions import IsOwnerOrReadOnly
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class GymViewSet(viewsets.ModelViewSet):
     queryset = Gym.objects.all()
@@ -11,6 +13,18 @@ class GymViewSet(viewsets.ModelViewSet):
 class WallViewSet(viewsets.ModelViewSet):
     queryset = Wall.objects.all()
     serializer_class = WallSerializer
+    
+    @action(detail=False)
+    def filter_gym(self, request):
+        gym_pk = request.query_params.get('gym_id')
+        queryset = Wall.objects.filter(gym_id=gym_pk)
+        if queryset.exists():
+            serializer = WallSerializer(queryset,
+                                        many=True,
+                                        context={'request': request})
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 class ClimbViewSet(viewsets.ModelViewSet):
     """
@@ -24,6 +38,18 @@ class ClimbViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(profile=self.request.user)
 
+    @action(detail=False)
+    def filter_wall(self, request):
+        wall_pk = request.query_params.get('wall_id')
+        queryset = Climb.objects.all().filter(wall_id=wall_pk)
+        if queryset.exists():
+            serializer = ClimbSerializer(queryset,
+                                         many=True,
+                                         context={'request': request})
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
 class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
@@ -36,3 +62,18 @@ class ClimbingLogViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(profile=self.request.user)
+
+    @action(detail=False)
+    def filter_user(self, request):
+        profile_pk = request.query_params.get('profile_id')
+        if profile_pk:
+            queryset = Climbing_Log.objects.filter(profile=profile_pk)
+        else:
+            queryset = Climbing_Log.objects.filter(profile=request.user.id)
+        if queryset.exists():
+            serializer = ClimbingLogSerializer(queryset,
+                                               many=True,
+                                               context={'request': request})
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
