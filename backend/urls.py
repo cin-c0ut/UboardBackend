8baadca3ff70d418bb211a4cb93f4441b22dc390
alex <awright13@bigpond.com>
2025-04-08 23:37:05 +1000

    Added swagger API documentation

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from backend.views import GymViewSet, WallViewSet, ClimbViewSet, ProfileViewSet, ClimbingLogViewSet
 
 router = DefaultRouter()
@@ -9,6 +12,17 @@ router.register(r'climbs', ClimbViewSet, basename='climb')
 router.register(r'profiles', ProfileViewSet, basename='profile')
 router.register(r'climbing_logs', ClimbingLogViewSet, basename='climbing_log')
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Uboard API",
+      default_version='v1',
+   ),
+   public=True,
+)
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
