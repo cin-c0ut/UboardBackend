87c2de396a1643f840af975e5f04f3b219d0a0b0
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2025-04-29 10:54:59 +1000

    Fix bug with creating user

--- backend/views/create_user_view.py
+++ backend/views/create_user_view.py
@@ -1,9 +1,9 @@
 from backend.serializers import CreateUserProfileSerializer
 from rest_framework import mixins, viewsets
-from django.conf import settings
+from django.contrib.auth.models import User
 
 class CreateUserViewSet(mixins.CreateModelMixin,
                         mixins.RetrieveModelMixin,
                         viewsets.GenericViewSet):
-    queryset = settings.AUTH_USER_MODEL.objects.all()
+    queryset = User.objects.all()
     serializer_class = CreateUserProfileSerializer
