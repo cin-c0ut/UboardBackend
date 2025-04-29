6f67fa282ac1ac73f3bd16c5755a0eff8f53df08
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2025-04-29 23:52:42 +1000

    Add update and delete user

--- backend/serializers.py
+++ backend/serializers.py
@@ -41,11 +41,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username']        
+        fields = ['id', 'username', 'password', 'first_name', 'last_name']        
+        read_only_fields = ['id', 'username']
+        extra_kwargs = {'password': {'write_only': True}}
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    user = UserSerializer(read_only=True)
+    user = UserSerializer()
+    climbs = serializers.HyperlinkedRelatedField(queryset=Climb.objects.all(),
+                                                 many=True,
+                                                 view_name='climb-detail')
     saved_gyms = serializers.HyperlinkedRelatedField(queryset=Gym.objects.all(),
                                                      many=True,
                                                      view_name='gym-detail')
@@ -58,12 +63,28 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'saved_gyms', 'saved_walls', 'saved_gyms', 'saved_climbs']
+        fields = ['id', 'user', 'saved_gyms', 'saved_walls', 'saved_climbs', 'climbs']
+        read_only_fields = ['id', 'saved_gyms', 'saved_walls', 'saved_climbs', 'climbs']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+
+        instance_user = instance.user
+        for field, value in user_data.items():
+            setattr(instance_user, field, value)
+
+        if user_data["password"]:
+            instance_user.set_password(user_data["password"])
+        instance_user.save()
+        instance.save()
+
+        return instance
+
 
 class CreateUserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
--- backend/views/profile_view.py
+++ backend/views/profile_view.py
@@ -1,7 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.response import Response
 from backend.models import Profile
 from backend.serializers import ProfileSerializer
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer 
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = instance.user
+
+        self.perform_destroy(instance)
+        user.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
