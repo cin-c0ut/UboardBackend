1a8a4afa94e89ae4a7e4e640a6626ccaa51c50aa
alex <awright13@bigpond.com>
2025-05-10 12:02:26 +1000

    need to add more to gyms

--- backend/models/gym.py
+++ backend/models/gym.py
@@ -4,3 +4,6 @@ class Gym(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
+    #Need to add more to this model 
+    #a flag for public or private to determine the get calls
+    #private gyms are personal gyms that one the user has access to 
