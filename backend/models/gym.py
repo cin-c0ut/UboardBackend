1f76565985bd0a649aa5f906435851357eabff2f
alex <awright13@bigpond.com>
2025-05-10 11:14:21 +1000

    woops

--- backend/models/gym.py
+++ backend/models/gym.py
@@ -3,4 +3,4 @@ from django.db import models
 class Gym(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200) 
+    location = models.CharField(max_length=200)
